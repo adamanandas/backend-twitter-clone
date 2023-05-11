005feb2adfb2dfca88c25ac0175767d2cdbf7616
adamanandas <adamananda02@gmail.com>
2023-05-11 15:32:47 +0700

    Add tweet model

---
+++ models/TweetModel.ts
@@ -0,0 +1,43 @@
+import { UUID } from "crypto";
+import sequelize from "../utils/database";
+import { Model, DataTypes, InferAttributes, InferCreationAttributes } from "sequelize";
+
+interface TweetModel extends Model<InferAttributes<TweetModel>, InferCreationAttributes<TweetModel>> {
+  id: UUID;
+  user_id: UUID;
+  text: string;
+  media: string;
+  likes_count: number;
+  comments_count: number;
+  retweets_count: number;
+}
+
+const TweetModel = sequelize.define<TweetModel>("tweet", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+  },
+  user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  text: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  media: {
+    type: DataTypes.STRING,
+  },
+  likes_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  comments_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  retweets_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+});
