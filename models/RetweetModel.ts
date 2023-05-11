5709d430db4603223ee00236445ca8ab83189dd2
adamanandas <adamananda02@gmail.com>
2023-05-11 15:49:25 +0700

    Add retweet model

---
+++ models/RetweetModel.ts
@@ -0,0 +1,24 @@
+import { UUID } from "crypto";
+import sequelize from "../utils/database";
+import { Model, DataTypes, InferAttributes, InferCreationAttributes } from "sequelize";
+
+interface RetweetModel extends Model<InferAttributes<RetweetModel>, InferCreationAttributes<RetweetModel>> {
+  id: UUID;
+  user_id: UUID;
+  tweet_id: UUID;
+}
+
+const TweetModel = sequelize.define<RetweetModel>("retweet", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+  },
+  user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  tweet_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+});
