0daab541aab21b30e6eb39ca021a415ef1a44576
adamanandas <adamananda02@gmail.com>
2023-05-12 13:47:15 +0700

    Add database config and connect to database utils

---
+++ src/config/database.ts
@@ -0,0 +1,7 @@
+const dbDriver = "mysql";
+const dbName = "twitter_clone";
+const dbHost = "localhost";
+const dbUser = "root";
+const dbPassword = "";
+
+export { dbDriver, dbName, dbHost, dbUser, dbPassword };
--- utils/database.ts
+++ utils/database.ts
@@ -1,8 +1,16 @@
 import { Sequelize } from "sequelize";
 
-const sequelize: Sequelize = new Sequelize("twitter_clone", "root", "", {
-  dialect: "mssql",
-  host: "localhost",
+import * as dbConfig from "../src/config/database";
+
+const dbDriver = dbConfig.dbDriver;
+const dbName = dbConfig.dbName;
+const dbHost = dbConfig.dbHost;
+const dbUser = dbConfig.dbUser;
+const dbPassword = dbConfig.dbPassword;
+
+const sequelize: Sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  dialect: dbDriver,
+  host: dbHost,
 });
 
 export default sequelize;
